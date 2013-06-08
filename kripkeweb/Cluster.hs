fef424bd490bf72f5a986dbe2c5025e4c7302a00
dawe <dawedawe@gmx.de>
2013-06-08 02:54:40 +0200

    add a version of kMeans that operates on SpacePnt data so the world names can be seen in the results

--- kripkeweb/Cluster.hs
+++ kripkeweb/Cluster.hs
@@ -15,6 +15,14 @@ import DB
 import KripkeTypes
 import LogicSearch
 import Model
+import Tfidf
+
+data SpacePnt = SpacePnt { name     :: T.Text
+                         , position :: [Bool]
+                         }
+
+instance Show SpacePnt where
+    show (SpacePnt n p) = show (n, show p)
 
 -- |fmlSpacePos vectors of all given worlds.
 fmlSpacePoses :: (AsLambdaType f, PTrueIn f) => Model -> [f] -> [T.Text] ->
@@ -22,16 +30,28 @@ fmlSpacePoses :: (AsLambdaType f, PTrueIn f) => Model -> [f] -> [T.Text] ->
 fmlSpacePoses mdl@(Model (Frame w r) _) fmls ws =
     map (fmlSpacePos mdl fmls) (S.toList w)
 
+-- |fmlSpacePos vectors of all given worlds.
+fmlSpacePoses' :: (AsLambdaType f, PTrueIn f) => Model -> [f] -> [T.Text] ->
+                 [SpacePnt]
+fmlSpacePoses' mdl@(Model (Frame w r) _) fmls ws =
+    map (fmlSpacePos' mdl fmls) (S.toList w)
+
 -- |Position of a world with regard to a list of formulas spanning a space.
 -- Expects the formulas to be already in the right LambdaType.
 fmlSpacePos :: (PTrueIn f) => Model -> [f] -> T.Text -> [Bool]
 fmlSpacePos mdl fmls w = map (isPTrueInWorld mdl w) fmls
 
+-- |Position of a world with regard to a list of formulas spanning a space.
+-- Expects the formulas to be already in the right LambdaType.
+fmlSpacePos' :: (PTrueIn f) => Model -> [f] -> T.Text -> SpacePnt
+fmlSpacePos' mdl fmls w = SpacePnt w (map (isPTrueInWorld mdl w) fmls)
+
+
 -- |Distance of two Bool Lists.
 boolsDist :: [Bool] -> [Bool] -> Double
 boolsDist x y
     | length x == length y = sqrt (fromIntegral (sum (zipWith boolDist x y)))
-    | otherwise            = error "boolsDist: lists of differing length"
+    | otherwise            = error "boolsDist: uneven lists"
 
 -- |Distance of two Bools.
 boolDist :: Bool -> Bool -> Int
@@ -59,7 +79,8 @@ randomBools n = do
     return (take n (randoms g))
 
 -- |kMeans implementation for a boolean n dimensional space.
-kMeans :: (AsLambdaType f, PTrueIn f) => Model -> [f] -> Int -> IO [[[Bool]]]
+kMeans :: (AsLambdaType f, PTrueIn f) => Model -> [f] -> Int ->
+          IO [([Bool], [[Bool]])]
 kMeans mdl@(Model (Frame w r) l) fmls k = do
     let dims   = length fmls
     centroids  <- mapM (\_ -> randomBools dims) [0..(k - 1)]
@@ -71,7 +92,7 @@ kMeans mdl@(Model (Frame w r) l) fmls k = do
     return (kMeansLoop clusters centroids k)
 
 -- |Looping function for kMeans, ends when centroids stop moving.
-kMeansLoop :: [[[Bool]]] -> [[Bool]] -> Int -> [[[Bool]]]
+kMeansLoop :: [[[Bool]]] -> [[Bool]] -> Int -> [([Bool], [[Bool]])]
 kMeansLoop clusters centroids k = do
     let hOfx       = map (closestCentroidIdx centroids) (concat clusters)
     let wk         = zip (concat clusters) hOfx
@@ -82,7 +103,35 @@ kMeansLoop clusters centroids k = do
     let centroids'' = keepEmptyClustCents centroids centroids'
     if centroids'' /= centroids
       then kMeansLoop clusters' centroids'' k
-      else clusters'
+      else zip centroids'' clusters'
+
+-- |kMeans implementation for a boolean n dimensional space.
+kMeans' :: (AsLambdaType f, PTrueIn f) => Model -> [f] -> Int ->
+          IO [([Bool], [SpacePnt])]
+kMeans' mdl@(Model (Frame w r) l) fmls k = do
+    let dims   = length fmls
+    centroids  <- mapM (\_ -> randomBools dims) [0..(k - 1)]
+    let wPoses = fmlSpacePoses' mdl fmls (S.toList w)
+    let hOfx   = map (closestCentroidIdx' centroids) wPoses
+    let wk     = zip wPoses hOfx
+    let clusters =
+          [map fst c | i <- [0..(k - 1)], let c = filter ((== i) . snd) wk]
+    return (kMeansLoop' clusters centroids k)
+
+-- |Looping function for kMeans, ends when centroids stop moving.
+kMeansLoop' :: [[SpacePnt]] -> [[Bool]] -> Int -> [([Bool], [SpacePnt])]
+kMeansLoop' clusters centroids k = do
+    let hOfx       = map (closestCentroidIdx' centroids) (concat clusters)
+    let wk         = zip (concat clusters) hOfx
+    let clusters'  =
+          [map fst c | i <- [0..(k - 1)], let c = filter ((== i) . snd) wk]
+    -- new centroids
+    let centroids' = map (sumBoolLists . map position) clusters'
+    -- deal with possible empty clusters and the resulting null-like centroids
+    let centroids'' = keepEmptyClustCents centroids centroids'
+    if centroids'' /= centroids
+      then kMeansLoop' clusters' centroids'' k
+      else zip centroids'' clusters'
 
 -- |Determine the index number of the closes centroid.
 closestCentroidIdx :: [[Bool]] -> [Bool] -> Int
@@ -93,10 +142,19 @@ closestCentroidIdx cs w =
     in
       fromJust (L.elemIndex minD dists)
 
+-- |Determine the index number of the closes centroid.
+closestCentroidIdx' :: [[Bool]] -> SpacePnt -> Int
+closestCentroidIdx' cs (SpacePnt _ pos) =
+    let
+      dists = map (boolsDist pos) cs
+      minD  = minimum dists
+    in
+      fromJust (L.elemIndex minD dists)
+
 -- |Take the corresponding element out of the first list if the element in the
 -- second list is null.
 keepEmptyClustCents :: [[a]] -> [[a]] -> [[a]]
 keepEmptyClustCents os ns
-    | length os /= length ns = error "keepEmptyClustCents: uneven lists given"
+    | length os /= length ns = error "keepEmptyClustCents: uneven lists"
     | otherwise              =
         map (\(o, n) -> if null n then o else n) (zip os ns)
