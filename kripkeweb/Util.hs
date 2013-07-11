c32792478efe18e11a5d2ebeb02ed4b28b504e84
dawe <dawedawe@gmx.de>
2013-07-11 21:19:35 +0200

    new functions appendUrlStrings, isStringAbsoluteHttpUrl, keepFirstXPercent

--- kripkeweb/Util.hs
+++ kripkeweb/Util.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Util
-( dropFirstAndLastXPercent
+( appendUrlStrings
+, dropFirstAndLastXPercent
 , eqListElems
 , hasLetters
+, isAbsoluteHttpUrl
 , isSoundExHash
+, isStringAbsoluteHttpUrl
+, keepFirstXPercent
 , lowerString
 , trim
 , unquote
@@ -12,6 +16,7 @@ module Util
 
 import Data.Char (isDigit, isLetter, isSpace, toLower)
 import qualified Data.Set as S
+import Network.URL
 
 -- |Transform a String to a lower case version.
 lowerString :: String -> String
@@ -54,6 +59,10 @@ dropFirstXPercent p xs =
 dropLastXPercent :: Int -> [a] -> [a]
 dropLastXPercent p xs = reverse $ dropFirstXPercent p (reverse xs)
 
+-- |Keep the first p percent of the list elements.
+keepFirstXPercent :: Int -> [a] -> [a]
+keepFirstXPercent p = dropLastXPercent (100 - p)
+
 -- |Convert p to p / 100.
 toPercent :: Int -> Double
 toPercent p = fromIntegral p / 100
@@ -64,3 +73,21 @@ isSoundExHash (c:x:y:z:[]) =
     c `elem` ['A' .. 'Z'] && isDigit x && isDigit y && isDigit z
 isSoundExHash _            = False
 
+-- |True, if URL is Absolute and the protocol is HTTP, otherwise False
+isAbsoluteHttpUrl :: URL -> Bool
+isAbsoluteHttpUrl (URL (Absolute (Host (HTTP _) _ _)) _ _) = True
+isAbsoluteHttpUrl _                                        = False
+
+-- |True, if String is an absolute HTTP URL, otherwise False
+isStringAbsoluteHttpUrl :: String -> Bool
+isStringAbsoluteHttpUrl str =
+      case importURL str of
+        Nothing -> False
+        Just u  -> isAbsoluteHttpUrl u
+
+-- |Deal with trailing, leading slashes.
+appendUrlStrings :: String -> String -> String
+appendUrlStrings u1 u2
+    | last u1 /= '/' && head u2 /= '/' = u1 ++ "/" ++ u2
+    | last u1 == '/' && head u2 == '/' = u1 ++ tail u2
+    | otherwise                        = u1 ++ u2
