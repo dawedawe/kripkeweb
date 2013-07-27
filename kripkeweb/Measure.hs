60093198e93595e04df58012dcdf8d14f66e54d4
dawe <dawedawe@gmx.de>
2013-07-28 00:09:21 +0200

    move similarity, disimilarity into Measure.hs for real

--- kripkeweb/Measure.hs
+++ kripkeweb/Measure.hs
@@ -1,6 +1,26 @@
 module Measure
-( edgesInDigraphClique
+( disimilarity
+, edgesInDigraphClique
+, similarity
 ) where
+
+import qualified Data.Set as S
+
+-- |Simple similarity measure: |x intersect y| / |x union y|.
+similarity :: (Eq a, Ord a) => S.Set a -> S.Set a -> Double
+similarity x y
+    | S.null x && S.null y = 1.0
+    | S.null x /= S.null y = 0.0
+    | otherwise            =
+        let
+          interSize = S.size (x `S.intersection` y)
+          unionSize = S.size (x `S.union` y)
+        in
+          fromIntegral interSize / fromIntegral unionSize
+
+-- |Simple disimilarity measure: 1 - similarity x y.
+disimilarity :: (Eq a, Ord a) => S.Set a -> S.Set a -> Double
+disimilarity x y = 1 - similarity x y
 
 -- |Edge count in a total directed graph.
 edgesInDigraphClique :: Int -> Int
